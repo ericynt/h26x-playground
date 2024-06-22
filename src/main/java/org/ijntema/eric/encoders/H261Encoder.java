e2f70266295957dc5a27fd758beb66dfc06ffc85
Eric IJntema <eric.ijntema@kpn.com>
2024-06-22 23:48:18 +0200

    Fix mistake.

--- src/main/java/org/ijntema/eric/encoders/H261Encoder.java
+++ src/main/java/org/ijntema/eric/encoders/H261Encoder.java
@@ -243,21 +243,11 @@ public class H261Encoder {
     public int[] zigzagOrderSequence (int[][] block) {
 
         int[] zigzag = new int[64];
-        int[][] zigzagOrder = {
-                {0, 1, 5, 6, 14, 15, 27, 28},
-                {2, 4, 7, 13, 16, 26, 29, 42},
-                {3, 8, 12, 17, 25, 30, 41, 43},
-                {9, 11, 18, 24, 31, 40, 44, 53},
-                {10, 19, 23, 32, 39, 45, 52, 54},
-                {20, 22, 33, 38, 46, 51, 55, 60},
-                {21, 34, 37, 47, 50, 56, 59, 61},
-                {35, 36, 48, 49, 57, 58, 62, 63}
-        };
         for (int i = 0; i < 8; i++) {
 
             for (int j = 0; j < 8; j++) {
 
-                zigzag[zigzagOrder[i][j]] = block[i][j];
+                zigzag[ZIGZAG_ORDER[i][j]] = block[i][j];
             }
         }
 
